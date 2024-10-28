dd947a6563a2560a4ce7236f672ac769e9028e2a
ACE <harshitrana@Harshits-MacBook-Air.local>
2024-10-28 14:23:11 +0530

    SNAKE - GAME

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Snake-Game
+This is a snake game project i made using java. 
---
+++ src/SnakeGame.java
@@ -0,0 +1,7 @@
+public class SnakeGame {
+    public static void main(String[] args) {
+        new GameFrame();
+        // we can also work with new GameFrame();
+    }
+
+}
